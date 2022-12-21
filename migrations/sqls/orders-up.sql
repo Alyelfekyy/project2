bf6e60a84139d34d4ac4d69d976d8aaf9fd436f6
alynamlaa <alynamlaa@gmail.com>
2022-12-22 01:58:55 +0200

    update migrations

--- migrations/sqls/orders-up.sql
+++ migrations/sqls/orders-up.sql
@@ -1,11 +1,5 @@
 CREATE TABLE orders (
-    order_id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
-    products integer[],
-    status varchar NOT NULL,
-    CONSTRAINT fk_orders_users
-        FOREIGN KEY (user_id)
-            REFERENCES users(id)
-            ON DELETE CASCADE
-            ON UPDATE CASCADE
+  id      SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL REFERENCES users (id),
+  status  BOOLEAN NOT NULL
 );
