5ad5c37258a36cf2a277ca19ea3512c77c96953f
alynamlaa <alynamlaa@gmail.com>
2023-02-12 22:06:41 +0200

    set up

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,18 @@
 import dotenv from 'dotenv'
-import { Pool } from 'pg'
+import { ClientConfig, Pool } from 'pg'
 
 dotenv.config()
-const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } =
-    process.env
 
-const client = new Pool({
-    host: POSTGRES_HOST,
-    database: POSTGRES_DB,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-})
+const config: ClientConfig = {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT as unknown as number,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+}
 
-export default client
+if (process.env.ENV === 'test') {
+    config.port = process.env.POSTGRES_PORT_TEST as unknown as number
+}
+
+export default new Pool(config)
